d2c39e204bbdbbec653d65173a19dbe22cd9442f
UGSNBandara <sulithanb119@gmail.com>
2024-10-24 00:17:45 +0530

    modified

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Protfolio-web1/",
+  base: "/Protfolio-web/",
   plugins: [react()],
 })
