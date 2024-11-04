59d694c6b2b1d5458ce7d40fbe5ef7e2914de605
U.G.Sulitha Nulaksha Bandara <138523990+UGSNBandara@users.noreply.github.com>
2024-11-04 15:31:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  basr: '/my-portfolio-1/'
+  base: '/my-portfolio-1/'
 });
